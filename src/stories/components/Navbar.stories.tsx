11dc8af6c677213ea15a8cb7ce4de491789fac71
Raul_camacho <osvaldoracm@gmail.com>
2022-01-13 20:54:22 -0600

    feat: deploy

--- src/stories/components/Navbar.stories.tsx
+++ src/stories/components/Navbar.stories.tsx
@@ -13,15 +13,15 @@ const Template: ComponentStory<typeof Navbar> =(args)=><Navbar {...args}/>
 export const Basic = Template.bind({});
 Basic.args = {
     // title:'No title',
-    logo:'../assets/logo_rcm.jpg',
-    alt:"Logo de ...",
-    backgroundColor: 'blue',
-    color:'white',
-    hoverColor:'black',
-    maxWidth:1460,
+    logo:'',
+    alt:"alt text",
+    backgroundColor: '',
+    color:'',
+    hoverColor:'',
+    maxWidth:1480,
     options:[
-        {id:1, title:'Home', url:'face', target:false},
-        {id:2, title:'About', url:'face2' , target:false},
+        {id:1, title:'Home', url:'#!', target:false},
+        {id:2, title:'About', url:'#!' , target:false},
         {id:3, title:'Google', url:'https://google.com' , target:true},
     ]
 }

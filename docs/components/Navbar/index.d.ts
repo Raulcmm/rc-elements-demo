da7eab1c6e1cb7c3ef4a288a65cada9957a1ba4b
Raul_camacho <osvaldoracm@gmail.com>
2022-01-13 19:19:43 -0600

    feat: First component -  navbar

---
+++ docs/components/Navbar/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="react" />
+import { PropsNavbarComponent } from './interfaces.navbar';
+export declare const Navbar: (props: PropsNavbarComponent) => JSX.Element;
+export default Navbar;
---
+++ docs/stories/components/Navbar.stories.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="react" />
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+declare const _default: ComponentMeta<(props: import("../../components/Navbar/interfaces.navbar").PropsNavbarComponent) => JSX.Element>;
+export default _default;
+export declare const Basic: ComponentStory<(props: import("../../components/Navbar/interfaces.navbar").PropsNavbarComponent) => JSX.Element>;
